a7f20ab2ee577851255eee37cb3815a538c70fcc
Jacob <Jakkebluben@hotmail.com>
2017-10-18 10:31:54 +0200

    Changed lappar.java

--- Utdelare_av_kölappar.java
+++ Utdelare_av_kölappar.java
@@ -9,8 +9,8 @@ public class Utdelare_av_kölappar {
         
         
         //Vad händer om vi skapar ett objekt till?
-        QueueTicketDispenser q2 = new QueueTicketDispenser();
-        myNumber = q2.getNextNumber();
+		
+        myNumber = q.getNextNumber();
         System.out.println("Könummer: " + myNumber);
         //Det andra objektet levererar samma serie nummer som det första,
         //lösning: skapa bara ett objekt av klassen
