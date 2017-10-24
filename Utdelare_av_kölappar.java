3abd89421d581720524994a39ad072f43ce0c6bc
Jacob <Jakkebluben@hotmail.com>
2017-10-24 16:56:16 +0200

    Diverse förbättringar

--- Utdelare_av_kölappar.java
+++ Utdelare_av_kölappar.java
@@ -1,20 +1,33 @@
-package Kölappsutdelare1;
+package QueueTicketV2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Utdelare_av_kölappar {
+    // hämta referensen till objektet med getInstance()
+    // och hämta nummret med getNextNumber()
 
     public static void main(String[] args) {
-        QueueTicketDispenser q = new QueueTicketDispenser();
-        int myNumber = q.getNextNumber();
-        System.out.println("Könummer: " + myNumber);
+        QueueTicketDispenser ticketDisp = QueueTicketDispenser.getInstance();
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        int n;
         
+        System.out.println("Skriv in hur många kölappar du vill skriva ut.");
+        while (true) {
+            try {
+                n = Integer.parseInt(stdin.readLine());
+                break;
+            } catch (IOException | NumberFormatException e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
         
-        //Vad händer om vi skapar ett objekt till?
-		
-        myNumber = q.getNextNumber();
-        System.out.println("Könummer: " + myNumber);
-        //Det andra objektet levererar samma serie nummer som det första,
-        //lösning: skapa bara ett objekt av klassen
+        System.out.println("Skriver ut kölappar...");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Kölapp: " + ticketDisp.getNextNumber());
+        }
         
-      
+        System.exit(0);
     }
 }
