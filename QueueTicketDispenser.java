ddb8dbabef6feed560d0a4b3b2106da6d08e189e
Jacob <Jakkebluben@hotmail.com>
2017-10-24 16:55:07 +0200

    Diverse förbättringar

--- QueueTicketDispenser.java
+++ QueueTicketDispenser.java
@@ -1,15 +1,30 @@
-package Kölappsutdelare1;
+package QueueTicketV2;
+// konstruktorn är private, ingen kan anropa den
+// skapa en objektreferns till QueueTicketDispenser och låt metoden
+// getInstance returnera detta enda objekt
 
 public class QueueTicketDispenser {
     private int nr;
     
-    public QueueTicketDispenser(){
+    private static QueueTicketDispenser theOne;
+    
+    public static QueueTicketDispenser getInstance () {
+        theOne = new QueueTicketDispenser();
+        return theOne;
+    }
+    
+    private QueueTicketDispenser () {
         nr = 1;
     }
     
-    public int getNextNumber(){
+    public int getNextNumber () {
         return nr++;
     }
-    
-    
 }
+
+// Fortfarande några svagheter:
+// Kölappsutdelaren initieras vare sig den används eller inte
+// Information som behövs för att initiera objektet kanske inte finns
+// tillgänglig när programmet startas och klassen laddas in.
+
+//***** Förbättra koden genom att ovanstående beaktas *****
